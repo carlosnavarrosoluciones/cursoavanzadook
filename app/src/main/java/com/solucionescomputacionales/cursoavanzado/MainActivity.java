bf98f7beadae69851e838ca81ae3d22be66959fe
carlos.navarrosoluciones@gmail.com <Puka2016$>
2020-08-06 20:12:18 -0500

    se crean menus desplegables usando toolbar

--- app/src/main/java/com/solucionescomputacionales/cursoavanzado/MainActivity.java
+++ app/src/main/java/com/solucionescomputacionales/cursoavanzado/MainActivity.java
@@ -4,16 +4,21 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import java.util.Objects;
 
 
 public class MainActivity extends AppCompatActivity {
 //// VISTAS
     Toolbar maintoolBar;
     Menu menuActivity;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
         maintoolBar=findViewById(R.id.toolbarMain);
         /// se indica cual action bar se usa
         setSupportActionBar(maintoolBar);
+       //para quitar el titulo del action Bar
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+        context=MainActivity.this;
     }
     ///llamar los tres puntitos infla la vista del menu que diseñamos (main menu ademas se iguala el menuActivity a la vista inflada
 
@@ -40,7 +48,10 @@ public class MainActivity extends AppCompatActivity {
         int id=item.getItemId(); // obtenemos el id del item presionado por el usuario
         switch (id){
             case R.id.mainMenu_logout:
-                Toast.makeText(this,"logout presionado",Toast.LENGTH_SHORT).show();
+                ///BORRAR SHARE PREFERENCES Y ENVIA A VENTANA DE CSRGA
+                myData myData = new myData(context);
+                myData.setLogeo(false);
+                startActivity(new Intent(context,LoadActivity.class));
                 break;
             case  R.id.mainMenu_About:
                 Toast.makeText(this,"acerca de",Toast.LENGTH_SHORT).show();
